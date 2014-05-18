cfa7cff3e4017a3c231bff4eebed17022e31a480
Linda Goldstein <lgoldste@thoughtworks.com>
2014-05-17 23:34:47 -0700

    WIP framework

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String... args) {
+        BillCalculator billCalculator = new BillCalculator();
+        billCalculator.addExpense(new Expense(amount, participants));
+        billCalculator.fromPerspective(participant);
+    }
+}
